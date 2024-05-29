bf15ac9e6c7ad0ef6ca57a3d22d5f8c38b781915
WMP399 <wmp3.99@gmail.com>
2024-05-28 20:54:27 -0500

    copied over pub/sub example pkg

---
+++ my_py_pkg/my_py_pkg/pub.py
@@ -0,0 +1,28 @@
+import rclpy
+from rclpy.node import Node
+from std_msgs.msg import String
+
+class Talker(Node):
+
+    def __init__(self):
+        super().__init__("publisher_node")
+        self.counter_ = 0
+        # create_publisher needs 3 parameters: Msg Type, message, and buffer size
+        self.publisher = self.create_publisher(String, "Hello", 10)
+        self._timer = self.create_timer(0.5, self.publish_hello)
+        self.get_logger().info("publishing message")
+
+    def publish_hello(self):
+        msg = String()
+        self.counter_ += 1
+        msg.data = "Hello World " + str(self.counter_)
+        self.publisher.publish(msg)
+
+def main(args=None):
+    rclpy.init(args=args)
+    node = Talker()
+    rclpy.spin(node)
+    rclpy.shutdown()
+
+if __name__ == "__main__":
+    main()
