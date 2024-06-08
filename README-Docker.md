a483c8e632240d713019a4a23e9d85ad9bce069d
David Sherrick <dsherrick@synthesissolutions.com>
2024-06-07 23:00:04 -0500

    Updating README.md and setup_pi.sh

---
+++ README-Docker.md
@@ -0,0 +1,33 @@
+# LaserGaze
+The LaserGaze system takes in data from a 4D LIDAR Scanner and provides auditory feedback to assist persons with visual impairments drive a vehicle. 
+
+## Deployment
+This program is intended to run on a Raspberry Pi 5, but to help accomodate as many potential deployment options as possible the system is contained within a docker container. Thus, any computer with USB host capabilities and the ability to run docker should be able to run the LaserGaze system. 
+
+## Dependencies
+Running LaserGaze requires a few depedencies - docker, docker-compose, the Unitree Robotics SDK, and the Unitree Robotics Point-LIO Example. Running the script setup_pi.sh will install these dependencies automatically. 
+
+## Building and Starting the Docker Image
+Running the script setup_pi.sh will install these dependencies automatically. To build the docker image manually, you can run the following terminal commands. Note the line that says `xhost local:root`. This allows any graphics inside the docker container to be piped out to the host via X11. This does not need to be run if data visualization is not needed or run on a seperate host.
+
+```console
+xhost local:root
+cd laser-gaze/docker-noetic
+docker build -t ros .
+docker-compose up -d
+docker exec -it docker-noetic_ros_1 bash
+```
+
+## Starting LaserGaze
+
+
+## Useful Links / References: 
+ROS Documentation: https://wiki.ros.org/ 
+
+Unitree Lidar L1 SDK: https://github.com/unitreerobotics/unilidar_sdk/tree/main
+
+Unitree LIO Implementation: https://github.com/unitreerobotics/point_lio_unilidar/tree/main
+
+Learn ROS Course by Kevin McAleer (including getting ROS running in Docker): https://www.kevsrobots.com/learn/learn_ros/
+
+Running a ROS Launcher Automatically on Boot: https://roboticsbackend.com/make-ros-launch-start-on-boot-with-robot_upstart/
--- setup_pi.sh
+++ setup_pi.sh
@@ -3,27 +3,49 @@ echo "check for updates and apply them"
 sudo apt update
 sudo apt upgrade -y
 
-# get laser-gaze files
-echo "get laser-gaze files"
-git clone https://github.com/wmp399/laser-gaze.git
+# Open firewall ports for ROS and SSH
+echo "open SSH and ROS ports in firewall"
+sudo ufw allow 22
+sudo ufw allow 11311
 
+# Install ROS Noetic
+echo "installing ROS Noetic"
+sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
+curl -s https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc | sudo apt-key add -
+sudo apt update
+sudo apt install ros-noetic-desktop
+echo "source /opt/ros/noetic/setup.bash" >> ~/.bashrc
+source ~/.bashrc
+sudo apt install python3-rosdep python3-rosinstall python3-rosinstall-generator python3-wstool build-essential
+sudo rosdep update
+sudo rosdep init
+
+# Install dependencies
+echo "install dependencies"
+sudo apt install ros-noetic-pcl*
+sudo apt install libsndfile1-dev
+sudo apt install libao-dev
+
+# get laser-gaze files
+echo "get laser-gaze files and submodules"
+git clone https://github.com/wmp399/laser-gaze.git
 cd laser-gaze
+git submodule update --init --recursive
+
 # get unitree lidar driver
 echo "get unitree lidar driver"
+cd
 git clone https://github.com/unitreerobotics/unilidar_sdk/tree/main
-cd ..
 
-# Get docker and install
-echo "Get docker and install"
-sudo apt-get install docker
-sudo apt-get install docker-compose
+# build unitree lidar driver
+echo "build unitree lidar driver"
+cd ~/unilidar_sdk/unitree_lidar_ros
+source devel/setup.bash
+catkin_make
 
-"Build ROS-Base"
-xhost local:root
-cd laser-gaze/docker-noetic
-docker build -t ros .
-
-docker-compose up -d
-
-echo "done."
-echo "type `docker run -it docker-noetic_ros_1` to start the container"
+# build LaserGaze
+echo "build LaserGaze"
+cd ~/laser-gaze/ros1_ws
+source devel/setup.bash
+source ~/unilidar_sdk/unitree_lidar_ros/devel/setup.bash --extend
+catkin_make
