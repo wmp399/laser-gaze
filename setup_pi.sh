99af2ae295d0c26dc5465d034719190afc321236
wmp399 <wmp3.99@gmail.com>
2024-06-08 00:01:06 -0500

    update setup script to account for unilidar sdk fork

--- setup_pi.sh
+++ setup_pi.sh
@@ -13,39 +13,35 @@ echo "installing ROS Noetic"
 sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
 curl -s https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc | sudo apt-key add -
 sudo apt update
-sudo apt install ros-noetic-desktop
+sudo apt install -y ros-noetic-desktop
 echo "source /opt/ros/noetic/setup.bash" >> ~/.bashrc
 source ~/.bashrc
-sudo apt install python3-rosdep python3-rosinstall python3-rosinstall-generator python3-wstool build-essential
+sudo apt install -y python3-rosdep python3-rosinstall python3-rosinstall-generator python3-wstool build-essential 
 sudo rosdep update
 sudo rosdep init
 
 # Install dependencies
 echo "install dependencies"
-sudo apt install ros-noetic-pcl*
-sudo apt install libsndfile1-dev
-sudo apt install libao-dev
+sudo apt install -y ros-noetic-pcl*
+sudo apt install -y libsndfile1-dev
+sudo apt install -y libao-dev
 
 # get laser-gaze files
 echo "get laser-gaze files and submodules"
-git clone https://github.com/wmp399/laser-gaze.git
+# git clone https://github.com/wmp399/laser-gaze.git
+# if you have this script, you have probably already cloned the laser-gaze repository.
 cd laser-gaze
 git submodule update --init --recursive
 
-# get unitree lidar driver
-echo "get unitree lidar driver"
-cd
-git clone https://github.com/unitreerobotics/unilidar_sdk/tree/main
-
-# build unitree lidar driver
-echo "build unitree lidar driver"
-cd ~/unilidar_sdk/unitree_lidar_ros
-source devel/setup.bash
+# build unitree lidar sdk
+echo "build unitree lidar sdk"
+cd ~/laser-gaze/unilidar_sdk/unitree_lidar_ros
 catkin_make
 
 # build LaserGaze
 echo "build LaserGaze"
 cd ~/laser-gaze/ros1_ws
-source devel/setup.bash
-source ~/unilidar_sdk/unitree_lidar_ros/devel/setup.bash --extend
 catkin_make
+
+source ~/laser-gaze/ros1_ws/devel/setup.bash
+source ~/laser-gaze/unilidar_sdk/unitree_lidar_ros/devel/setup.bash --extend
